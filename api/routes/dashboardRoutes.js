b9d3aa6c427a4c5674e5857326b9e77d4370f4d2
Omaida Gonzalez <omi.gongar@gmail.com>
2019-02-27 13:28:08 +0100

    Added basic dashboard jobs, dashboard model, controllers and routes

--- api/routes/dashboardRoutes.js
+++ api/routes/dashboardRoutes.js
@@ -4,25 +4,26 @@ const express = require('express');
 const router = express.Router();
 var dashboard = require('../controllers/dashboardController');
 
-/**
-   * Get trips information:
-   *    RequiredRoles: Administrator
-   *
-   * @section dashboards
-   * @type get 
-   * @url /v1/dashboards/general
-  */
- router.get('/v1/dashboards/general', dashboard.get_dasboard)
-
  /**
-   * Compute cube:
-   *    RequiredRoles: Administrator
-   *
-   * @section dashboard
-   * @type get 
-   * @url /v1/dashboards/compute-cube
-   * @param {string} period
+	 * Get a list of all indicators or post a new computation period for rebuilding
+	 * RequiredRole: Administrator
+	 * @section dashboard
+	 * @type get post
+	 * @url /dashboard
+	 * @param [string] rebuildPeriod
+	 * 
   */
- router.get("/v1/dashboards/compute-cube", dashboard.compute_cube);
+router.get('/v1/dashboards', dashboard.list_all_indicators); 
+router.post('/v1/dashboards', dashboard.rebuildPeriod);
+	
+	/**
+	 * Get a list of last computed indicator
+	 * RequiredRole: Administrator
+	 * @section dashboard
+	 * @type get
+	 * @url /dashboard/latest
+	 * 
+	*/
+	router.get('/v1/dashboards/latest', dashboard.last_indicator);
 
  module.exports = router;
