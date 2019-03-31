b0b1ba5bdecf6ab0e0f0b3c2421ab481a29217ce
hector23rp <hector23rp@gmail.com>
2019-03-31 15:05:08 +0200

    Added MongoHOST

--- start.sh
+++ start.sh
@@ -5,6 +5,7 @@ BASE_SITE=acme-explorer.com
 export NODE_ENV=development
 export PORT=8008
 export DBPORT=27018
+export DBHOST=mongo
 export VIRTUAL_HOST=$NODE_ENV.$BASE_SITE
 docker-compose -p $VIRTUAL_HOST up -d --build
 
@@ -12,5 +13,6 @@ docker-compose -p $VIRTUAL_HOST up -d --build
 export NODE_ENV=production
 export PORT=8001
 export DBPORT=27011
+export DBHOST=mongo
 export VIRTUAL_HOST=$BASE_SITE
 docker-compose -p $VIRTUAL_HOST up -d --build
